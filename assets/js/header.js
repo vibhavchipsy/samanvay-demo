d60ce4db04dd14c949e2c3746d1fd30212619798
vibhav <kamathpvk@gmail.com>
2024-11-25 06:05:12 +0530

    header change

--- assets/js/header.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const toggleButton = document.querySelector('.menu-toggle');
-    const menu = document.querySelector('.menu ul');
-
-    toggleButton.addEventListener('click', () => {
-        menu.classList.toggle('active');
-    });
-});
